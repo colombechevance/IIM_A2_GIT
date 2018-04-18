7c4b207cb65cd1e51a21b02ad043a691a051f7e7
Colombe Chevance <chevanccolombe@gmail.com>
2018-04-18 14:20:16 +0200

    Validation merge

--- validation.php
+++ validation.php
@@ -7,8 +7,19 @@ if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['passwor
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
 	// TODO
+        $username = htmlspecialchars($_POST['username']);
+        $email = htmlspecialchars($_POST['email']);
+        $password = htmlspecialchars($_POST['password']);
 
-}else{ 
+        if(isUsernameAvailable($db, $username)) {
+               if(isEmailAvailable($db, $email)) {
+                       userRegistration($db, $username, $email, $password);
+                    } else {
+                       $error = "Email indisponible"; }
+    } else {
+            $error = "Username indisponible"; }
+
+}else {
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
 	header('Location: register.php');
 }
